041194aa2abdbc2925cac494ad1fd584d6c70e62
tteichmann <tyler.teichmann@stmartin.edu>
2024-03-19 11:30:31 -0700

    feat: add selection sort method

--- Project.java
+++ Project.java
@@ -11,7 +11,8 @@ public class Project {
         }
         System.out.println();
 
-        BubbleSort(arr);
+        // BubbleSort(arr);
+        SelectionSort(arr);
 
         for (int value : arr) {
             System.out.print(value + ", ");
@@ -21,13 +22,32 @@ public class Project {
 
     public static void BubbleSort (int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length -1; j++) {
-                if (arr[j] > arr[j+ 1]) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int tmp = arr[j];
-                    arr[j] = arr[j+1];
+                    arr[j] = arr[j + 1];
                     arr[j+1] = tmp;
                 }
             }
         }
+    }
+
+    public static void SelectionSort(int[] arr) {
+        int min, temp;
+
+        for (int i = 0; i < arr.length; i++) {
+            min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
     }
 }
